10c2aa9ba9790831ad107586f2ae14a8e4d7e885
Persian5 <iranopedia5@gmail.com>
2025-03-14 18:06:04 -0700

    Update flashcardscript.js

--- docs/flashcardscript.js
+++ docs/flashcardscript.js
@@ -2,3 +2,9 @@ function flipCard(card) {
     card.classList.toggle('flipped');
 }
 
+// Function to play the correct MP3 file
+function playAudio(file, event) {
+    event.stopPropagation(); // Prevent flip when clicking the sound button
+    let audio = new Audio(`../audios1/${file}`); // Adjusted to match your structure
+    audio.play();
+}
